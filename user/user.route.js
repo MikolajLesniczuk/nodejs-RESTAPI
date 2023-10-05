68e41c83df9c891e041b9c3b3bbe1eddcc97f513
MikolajLesniczuk <119354331+MikolajLesniczuk@users.noreply.github.com>
2023-10-05 22:44:16 +0200

    final

--- user/user.route.js
+++ user/user.route.js
@@ -1,3 +1,4 @@
+const User = require("./user.model");
 const express = require("express");
 const router = express.Router();
 const { authMiddleware } = require("../authorizarion/auth.midleware");
@@ -6,11 +7,21 @@ const {
   login,
   logout,
   getCurrentUser,
-} = require("../authorizarion/auth.controler");
+  getUsers,
+  uploadAvatar,
+} = require("./user.controler");
+const path = require("path");
+const multer = require("multer");
+
+const upload = multer({
+  dest: path.join(__dirname, "tmp"),
+});
 
 router.post("/signup", register);
 router.post("/login", login);
 router.post("/logout", authMiddleware, logout);
 router.get("/current", authMiddleware, getCurrentUser);
+router.get("/all", getUsers);
+router.patch("/avatars", upload.single("avatar"), uploadAvatar);
 
 module.exports = router;
